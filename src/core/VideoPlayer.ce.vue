975d97c9825cd6b86255335a059338c1ec33aa6e
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2022-10-02 23:13:43 +0300

    feat: init project structure, config linters and vite

---
+++ src/core/VideoPlayer.ce.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const videoElementRef = ref<HTMLVideoElement>();
+</script>
+
+<template>
+  <div class="lpr">
+    <video class="lpr__video" ref="videoElementRef"></video>
+
+    <div class="lpr__container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
